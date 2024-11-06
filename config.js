f5b922222d2cb65259da6d36e55da8e0c99248e6
wonjin1234 <wj4302@gmail.com>
2024-11-06 20:12:22 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
+export const WEDDING_DATE = "2025년 03월 08일, 목요일 오전 12시 00분";
 export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
 
-export const GROOM_NAME = "○○○";
+export const GROOM_NAME = "이원진";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_FATHER_NAME = "○○○";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
